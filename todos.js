a62455fd61241a2ac3ef11f27300715796840c79
NightmanCZ90 <novak.jan90@gmail.com>
2020-04-10 21:31:16 +0200

    Completed

--- todos.js
+++ todos.js
@@ -1,8 +1,18 @@
 $(".add").on("click", () => {
   addTodo = $("input").val(); //input value
-  $("ul").append(`<li>${addTodo}</li>`); // add the value in the list
+  $("ul").append(`<li><span class="todo-text">${addTodo}</span><span><i class="fas fa-trash"></i><i class="fas fa-check-circle"></i></span></li>`); // add the value in the list
   $("input").val(""); // delete value from input
 });
-$("ul").on("click", "li", function(){
-  $(this).toggleClass("scratched");
+
+$("ul").on("click", "li .todo-text", function(){ // CORRECT
+  $(this).toggleClass("scratched"); // CORRECT
+})
+
+$("ul").on("click", ".fa-check-circle", function(){
+  $(this).parent().parent().toggleClass("scratched");
+  $(this).toggleClass("green");
+})
+
+$("ul").on("click", ".fa-trash", function(){
+  $(this).parent().parent().remove();
 })
